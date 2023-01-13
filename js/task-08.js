fa7cc97fc90d36e97c3dc4dbfb209bcb11f5a728
Denis <ghntrdo@gmail.com>
2023-01-13 11:58:58 +0300

    js

--- js/task-08.js
+++ js/task-08.js
@@ -4,36 +4,32 @@ const onFormSubmit = event => {
   // отмена настройки по умолчанию (перезагрузки страницы при отправке формы)
   event.preventDefault();
 
+  // деструктуризация <input name="email" <input name="password"
   const {
     elements: { email, password },
   } = event.currentTarget;
 
-  // event => currentTarget => elements => email => value;
-  // event => currentTarget => elements => email => password;
-
+  // если поля незаполнены - alert (заполните форму)
   if (email.value === '' || password.value === '') {
     alert('Please fill in all the fields!');
+
+    // если поля заполнены правильно - даные выводятся в консоль
+  } else {
+    const formData = new FormData(event.currentTarget);
+    formData.forEach((value, name) => {
+      console.log(`${name}: ${value}`);
+    });
+    // очищает значения полей после отправки формы (если все поля заполнены)
+    event.currentTarget.reset();
   }
-
-  // 1 сбор выборочных данных
-  // const formElements = event.currentTarget.elements;
-  // const emailValue = formElements.email.value;
-  // const passwordValue = formElements.password.value;
-
-  // const formElementsData = {
-  //   emailValue,
-  //   passwordValue,
-  // };
-  // console.log(formData);
-  //
-  // 2 сбор всех данных
-  const formData = new FormData(event.currentTarget);
-  formData.forEach((value, name) => {
-    console.log(`${name}: value`);
-  });
-
-  // очищает значения полей после отправки формы
-  event.currentTarget.reset();
 };
 
 formEl.addEventListener('submit', onFormSubmit);
+
+// event => currentTarget => elements => email => value;
+// event => currentTarget => elements => email => password;
+
+// 1 сбор выборочных данных
+// const formElements = event.currentTarget.elements;
+// const emailValue = formElements.email.value;
+// const passwordValue = formElements.password.value;
--- js/task-09.js
+++ js/task-09.js
@@ -1,18 +1,34 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-// создаю обьект {свойство-это элемент HTML, значение-это элемент JS(через querySelector) }
+// создаю обьект ссылок на элементы {свойство-это элемент HTML, значение-это элемент JS(через querySelector) }
 const refs = { body: document.querySelector('body'), button: document.querySelector('.change-color'), span: document.querySelector('.color') };
 
 const onButtonClick = () => {
   // цвет фона
-  refs.body.style.backgroundColor = getRandomHexColor();
-  // изменение текста(название цвета)
-  refs.span.textContent = getRandomHexColor();
+  let color = getRandomHexColor();
+  refs.body.style.backgroundColor = color;
+  // изменение текста(название цвета) - HEX
+  refs.span.textContent = color;
+  // rgb
+  // refs.span.textContent = refs.body.style.backgroundColor;
 };
 
 refs.button.addEventListener('click', onButtonClick);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
+//
 // 2
 // const container = document.querySelector('.widget');
 
